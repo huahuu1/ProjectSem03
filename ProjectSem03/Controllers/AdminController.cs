063ce7bbc8a0e31a801156717e5f9edac4d2f6d7
huuht <huahuu1@gmail.com>
2020-09-22 23:36:39 +0700

    update counting

--- ProjectSem03/Controllers/AdminController.cs
+++ ProjectSem03/Controllers/AdminController.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SmartBreadcrumbs.Attributes;
+using ProjectSem03.Models;
 
 namespace ProjectSem03.Controllers
 {
     public class AdminController : Controller
     {
+        //connection to database
+        ProjectDB db;
+        public AdminController(ProjectDB db)
+        {
+            this.db = db;
+        }
+
         [DefaultBreadcrumb("Home")]
         public IActionResult Index()
         {
+            //Count data in models
+            ViewBag.countStaff = db.Staff.Count();
+            ViewBag.countStudent = db.Student.Count();
+            ViewBag.countCompetition = db.Competition.Count();
+            ViewBag.countExhibition = db.Exhibition.Count();
             return View();
         }
     }
